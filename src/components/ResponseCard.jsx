477297b9f707838862889d74197ba16f9c9414f0
codebysid <codebysidd@gmail.com>
2024-04-29 01:13:33 +0530

    check

--- src/components/ResponseCard.jsx
+++ src/components/ResponseCard.jsx
@@ -4,7 +4,7 @@ import CopyFormUrl from './CopyFormUrl'
 
 const ResponseCard = ({ formName, formId }) => {
   return (
-    <div className='flex w-full items-center justify-evenly lg:justify-center md:justify-center md:gap-10'>
+    <div className='flex w-full items-center justify-evenly lg:justify-center md:gap-10'>
       <h1 className='text-left w-[20%] break-words'>{formName}</h1>
       <CopyFormUrl createdFormId={formId} onlyFormUrl={true} />
       <CustomAlert formId={formId} />
